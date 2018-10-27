7e752c0fc238549702773f1e4c1b77c119fbb136
Addison Elliott <addison.elliott@gmail.com>
2018-10-26 23:50:47 -0500

    Continue making changes to get the program running

--- test/index.test.js
+++ test/index.test.js
@@ -3,13 +3,46 @@ var isMongodbRunningPromises = require('../promise');
 var assert = require('assert');
 
 describe('is-mongodb-running', function() {
-  it('should work', function() {
+  before(require('mongodb-runner/mocha/before'));
+  after(require('mongodb-runner/mocha/after'));
+  it('should exist', function() {
     assert(isMongodbRunning);
+  });
+
+  it('should work', function() {
+
+    // isMongodbRunning(args, function(err, res) {
+    //   // test with json and no-json
+    //   // test with port, pid, and none
+
+    //   // assert
+    //   // assert no err
+    //   // assert that it is running
+    //   if (err) {
+    //     if (args.json) {
+    //       err = JSON.stringify(err, null, 2);
+    //     }
+    //     console.error(chalk.red(figures.cross), err.message); // eslint-disable-line no-console
+    //     console.error(chalk.gray(err.stack)); // eslint-disable-line no-console
+    //     process.exit(1);
+    //     return;
+    //   }
+
+    //   if (args.json) {
+    //     err = JSON.stringify(err, null, 2);
+    //   }
+    //   console.error(chalk.red(figures.cross), err.message); // eslint-disable-line no-console
+    //   console.error(chalk.gray(err.stack)); // eslint-disable-line no-console
+    //   process.exit(1);
+
+    // // assert(isMongodbRunningPromises);
   });
 });
 
 describe('is-mongodb-running-with-promises', function() {
-  it('should work', function() {
+  before(require('mongodb-runner/mocha/before'));
+  after(require('mongodb-runner/mocha/after'));
+  it('should exist', function() {
     assert(isMongodbRunningPromises);
   });
 });
